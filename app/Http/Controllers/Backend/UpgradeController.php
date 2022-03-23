7ca02695162d016c975963dc33a4212308f6d291
crypt0k1ng13 <crypt0k1ng13@protonmail.com>
2022-03-23 22:48:45 +0500

    first commit

---
+++ app/Http/Controllers/Backend/UpgradeController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by NiNaCoder.
+ * Date: 2019-05-25
+ * Time: 09:01
+ */
+
+namespace App\Http\Controllers\Backend;
+
+use Illuminate\Http\Request;
+use DB;
+use View;
+
+class UpgradeController
+{
+    private $request;
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+
+    public function index(Request $request)
+    {
+        return view('backend.upgrade.index');
+    }
+
+
+    public function checkingLicense(Request $request)
+    {
+        $this->request->validate([
+            'license' => 'required|string',
+        ]);
+
+
+       return view('backend.upgrade.process');
+    }
+}
